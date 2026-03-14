68a1dc6943d450b8796d75337b24847313b9ca09
Wilder Chavez <wili.525186@gmail.com>
2026-03-14 15:55:56 -0500

    refactor: minor updates

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,9 +5,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideClientHydration(withEventReplay()),
-  ],
+    providers: [
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideRouter(routes),
+        provideClientHydration(withEventReplay()),
+    ],
 };
