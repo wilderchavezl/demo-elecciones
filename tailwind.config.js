358d3924f8b8402e5dc9b9a75b633d7a46ec88b8
Wilder Chavez <wili.525186@gmail.com>
2026-03-14 17:34:51 -0500

    feature: add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/**
+ * Tailwind configuration
+ */
+const config = {
+    content: ['./src/**/*.{html,scss,ts}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
+module.exports = config
+
