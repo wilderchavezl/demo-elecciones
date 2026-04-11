3c23f43af1d66fa9c342fb26b6e052d164ede040
Wilder Chavez <wili.525186@gmail.com>
2026-04-11 13:15:40 -0500

    refactor: update components

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
-    imports: [],
+    imports: [MatCardModule, MatButtonModule, RouterLink],
 })
 export class HomeComponent {}
