a71307bc98011fe3502554c6cd17826d11ec9395
Wilder Chavez <wili.525186@gmail.com>
2026-04-16 11:36:09 -0500

    refactor: minor updates

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,6 @@
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { provideCore } from '@core/core.provider';
@@ -10,6 +12,8 @@ export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes),
+        provideClientHydration(withEventReplay()),
+        provideHttpClient(withFetch()),
         provideAnimations(),
 
         // Theme
